d63dfb747309a91e6a629ef1ebfcb9774090c88c
nishant666 <nishants440@gmail.com>
2020-08-29 09:59:39 +0530

    v1

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
- //Your Firebase config!
+    apiKey: "AIzaSyD8OsvbP3uwjwXINjnnkjWpL91xn2Vaic4",
+    authDomain: "savvy-pagoda-275511.firebaseapp.com",
+    databaseURL: "https://savvy-pagoda-275511.firebaseio.com",
+    projectId: "savvy-pagoda-275511",
+    storageBucket: "savvy-pagoda-275511.appspot.com",
+    messagingSenderId: "945386535919",
+    appId: "1:945386535919:web:b82d5ad2c8f11e2ab3141a",
+    measurementId: "G-5PB9KQ3JXF"
   };
  
   firebase.initializeApp(firebaseConfig);
